28fa3ce3b9c693d337663844ea81c34a4f1d4fda
Pankaj-Surya <pnkjsrvnsh@gmail.com>
2023-02-24 11:50:54 +0530

    Added Home Card : home explore sub

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Card from "../components/Card";
+//import {API_URL} from "../config/clientConfig"
 
 const Container = styled.div`
   display: flex;
@@ -8,29 +10,24 @@ const Container = styled.div`
   flex-wrap: wrap;
 `;
 
-const Home = () => {
+const Home = ({type}) => {
+  const [videos,setVideos] = useState([])
+  
+  useEffect(()=>{
+    const fetchVideos = async ()=>{
+      const res = await axios.get(`${process.env.REACT_APP_API_URL}/videos/${type}`)
+      console.log(res.data)
+      setVideos(res.data)
+    }
+    fetchVideos()
+  },[type])
   return (
     <Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+     {
+       videos.map((video)=>(
+          <Card key={video._id} video={video}/>
+       ))
+     }
     </Container>
   );
 };
